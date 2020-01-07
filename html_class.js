fcfa90427c0a0e4d6fab94f687c0cd10cdd94933
Rodrigo Dittmar <36969389+rdittmarbr@users.noreply.github.com>
2020-01-07 13:32:38 -0300

    Create html_class.js

---
+++ html_class.js
@@ -0,0 +1,16 @@
+// Get all the elements in the document using querySelectorAll, then loop through them, getting each one's class, breaking it apart on spaces, and adding new ones to an allClasses array:
+
+var allClasses = [];
+
+var allElements = document.querySelectorAll('*');
+
+for (var i = 0; i < allElements.length; i++) {
+  var classes = allElements[i].className.toString().split(/\s+/);
+  for (var j = 0; j < classes.length; j++) {
+    var cls = classes[j];
+    if (cls && allClasses.indexOf(cls) === -1)
+      allClasses.push(cls);
+  }
+}
+
+console.log(allClasses);
