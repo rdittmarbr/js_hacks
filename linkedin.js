f756b8de90d3cf1876ca43d8c13962e164c76bde
rdittmar <rodrigo.dittmar@gmail.com>
2020-07-22 23:01:08 -0300

    Hacks

---
+++ linkedin.js
@@ -0,0 +1,8 @@
+/*  Adicionando todos os convites pendentes
+
+*/
+document
+  .querySelectorAll("ul.mn-invitation-list button.artdeco-button--secondary")
+  .forEach((e) => {
+    e.click();
+  });
